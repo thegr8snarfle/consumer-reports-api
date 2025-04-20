25c6f529e7eb9b07919d52a167a0747cf3d6c0be
Austin Kulp <aklimited.comapny@gmail.com>
2025-04-20 16:30:00 -0600

    Add logging infra.

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -19,15 +19,39 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_uri  = var.lambda_function_arn
 }
 
+#default route map
 resource "aws_apigatewayv2_route" "default" {
   api_id          = aws_apigatewayv2_api.http_api.id
   route_key       = "$default"
   target          = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
+#logging
+resource "aws_cloudwatch_log_group" "http_api_logs" {
+  name              = "/aws/http-api/${aws_apigatewayv2_api.http_api.id}"
+  retention_in_days = 3
+  tags              = var.tags
+}
+
+#state environment
 resource "aws_apigatewayv2_stage" "prod" {
   api_id          = aws_apigatewayv2_api.http_api.id
   name            = "prod"
   auto_deploy     = true
   tags            = var.tags
+
+    # Turn on execution logging & data trace
+  default_route_settings {
+    logging_level      = "ERROR"   # INFO | ERROR | OFF
+    data_trace_enabled = true     # include request/response bodies
+  }
+
+  # Access logs: each request → one log line
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.http_api_logs.arn
+    format = <<EOF
+      $context.requestId $context.identity.sourceIp $context.requestTime "$context.httpMethod $context.resourcePath $context.protocol" $context.status $context.responseLength
+    EOF
+  }
 }
+
--- terraform/modules/api-lambda/main.tf
+++ terraform/modules/api-lambda/main.tf
@@ -57,3 +57,8 @@ resource "aws_lambda_function" "lambda" {
   role            = aws_iam_role.lambda_role.arn
   tags            = var.tags
 }
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  retention_in_days = 3
+}
